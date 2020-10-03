5cca1d349015806f92115e0d4f4c54f1d23c6a9e
RoryGaliano <rorygaliano@gmail.com>
2020-10-03 23:06:04 +0000

    * bring it back

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 
 (function(window, _) {
   window.gamz = window.gamz || {
-    numz: {/* other code */},
+    numz: {
+        getAngleDegrees(pointA, PointB) {
+            const
+                distanceX = pointB.x - pointA.x,
+                distanceY = pointB.y - pointA.y,
+                radians = Math.atan2(distanceY, distanceX),
+                degrees = radians * 180 / Math.PI;
+            return degrees;
+        },
+        degreesToRadians(degrees) {
+            return degrees * Math.PI / 180;
+        },
+        radiansToDegrees(radians) {
+            return radians * 180 / Math.PI;
+        }
+    },
     phyz: {
       /**
        * Returns an Object with basic properties utilized in a 
